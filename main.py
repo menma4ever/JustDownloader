77c0281854a0ba17a96095c427c96112c936a62d
menma4ever <rahimovabdelaziz@gmail.com>
2024-12-29 22:37:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 bot = telebot.TeleBot('7319299432:AAFckpwgsiXqUKQGE7xmNdBYYct4faZg1ow')
 
 #from keep_alive import keep_alive
-keep_alive()
+#keep_alive()
 # Foydalanuvchi ma'lumotlari fayli yo'li
 user_data_file = 'user_data.json'
 
