664191055bb92812237b9c3d6e538e58314a3235
menma4ever <rahimovabdelaziz@gmail.com>
2024-12-29 22:52:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import json
 import os
 from datetime import datetime, timedelta
 import time
-
+from keep_alive import keep_alive
+keep_alive()
 
 # Bot tokeningizni kiritishingiz kerak
 bot = telebot.TeleBot('7319299432:AAFckpwgsiXqUKQGE7xmNdBYYct4faZg1ow')
+bot = Bot(token=os.environ.get('7319299432:AAFckpwgsiXqUKQGE7xmNdBYYct4faZg1ow'))
 
-#from keep_alive import keep_alive
-#keep_alive()
 # Foydalanuvchi ma'lumotlari fayli yo'li
 user_data_file = 'user_data.json'
 
